9a3b169aea820bd0030f3b0063c69642f6304d4b
Galina <gana88tit@gmail.com>
2023-11-04 10:48:28 +0300

    change src

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -6,4 +6,13 @@ public class CashbackHackService {
     public int remain(int amount) {
         return boundary - amount % boundary;
     }
+
+    @Test
+    public void testRemain1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 600;
+        int actual = service.remain(amount);
+        int expected = 400;
+        assertEquals(actual, expected);
+    }
 }
